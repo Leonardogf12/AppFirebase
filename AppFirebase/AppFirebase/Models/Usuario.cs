f44e0ee3f8aa336514dde3b3f6edeb9c645b5be4
Leonardogf12 <73491267+Leonardogf12@users.noreply.github.com>
2021-06-09 21:52:46 -0300

    Adicionar arquivos de projeto.

---
+++ AppFirebase/AppFirebase/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppFirebase.Models
+{
+    public class Usuario
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ AppFirebase/AppFirebase/Services/UsuarioService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppFirebase.Services
+{
+    public class UsuarioService
+    {
+
+    }
+}
